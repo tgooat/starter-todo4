db9481ddeb70665ac426f3bc784844f82564ee5d
Connor Goudie <connorgoudie@live.com>
2017-10-19 13:38:18 -0700

    Minor fix to styling.

--- application/views/template_secondary.php
+++ application/views/template_secondary.php
@@ -18,12 +18,12 @@
             <div id="content">
                 <h1>{pagetitle}</h1>
                 <div class="row">
-                    <div class="col-lg-6 col-md-12">
-                        {leftside}
-                    </div>
-                    <div class="col-lg-6 col-md-12">
-                        {rightside}
-                    </div>
+                    <div class="span6">
+                         {leftside}
+                     </div>
+                     <div class="span6">
+                         {rightside}
+                     </div>
                 </div>
             </div>
             <div id="footer" class="span12">
