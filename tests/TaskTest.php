520ca4e94c1ad5d92b3252bcce8cd73c7f0a4dbd
Connor Goudie <connorgoudie@live.com>
2017-11-03 20:13:36 -0700

    Finished tasktest unit testing, added travis ci support

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,5 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
+
 require '../application/entities/Task.php';
 
  class TaskTest extends TestCase {
@@ -8,9 +9,29 @@ require '../application/entities/Task.php';
   {
       $this->task = new Task();
   }
-  public function testSetTask()
+  public function testTaskSuccess()
   {
-     $taskIsSet = $this->task->task = "fuck";
+     $taskIsSet = $this->task->setTask('connor');
+     $groupIsSet = $this->task->setGroup(2);
+     $sizeIsSet = $this->task->setSize(3);
+     $priorityIsSet = $this->task->setPriority(4);
+     
      $this->assertTrue($taskIsSet);
+     $this->assertTrue($groupIsSet);
+     $this->assertTrue($sizeIsSet);
+     $this->assertTrue($priorityIsSet);
+  }
+  
+  public function testTaskFailure()
+  {
+     $taskIsSet = $this->task->setTask('Thisis64charactersitsgoingtotakemalongtimetotypebutineedtoseeifitworksthisisareallylongstringjimisacoolguyisthis64charactersyet');
+     $groupIsSet = $this->task->setGroup(10);
+     $sizeIsSet = $this->task->setSize(100);
+     $priorityIsSet = $this->task->setPriority(999999999);
+     
+     $this->assertFalse($taskIsSet);
+     $this->assertFalse($groupIsSet);
+     $this->assertFalse($sizeIsSet);
+     $this->assertFalse($priorityIsSet);
   }
 }
